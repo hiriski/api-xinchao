ca392e29c90dbcd7131258732b54d5c31f9853aa
riskiwebid <riski.web.id@zoho.com>
2020-09-14 01:42:03 +0700

    Add phrasebook migration and seeder

---
+++ database/migrations/2020_09_13_080016_create_phrasebook_categories_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhrasebookCategoriesTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('phrasebook_categories', function (Blueprint $table) {
+            $table->tinyIncrements('id');
+            $table->string('title');
+            $table->string('slug')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('phrasebook_categories');
+    }
+}
---
+++ database/migrations/2020_09_13_093119_create_phrasebooks_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhrasebooksTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('phrasebooks', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_ID')->comment('Bahasa Indonesia');
+            $table->string('vi_VN')->comment('Tiếng Việt');
+            $table->string('en_US')->nullable()->comment('(Optional) American English');
+            $table->text('notes')->nullable();
+            $table->unsignedBigInteger('created_by')->comment('Creator of this phrase');
+            $table->unsignedBigInteger('updated_by')->nullable()
+                ->comment('User who renew of this phrase');
+            $table->unsignedTinyInteger('category_id');
+
+            $table->softDeletes();
+            $table->timestamps();
+            
+            $table->foreign('created_by')
+                ->references('id')->on('users')->onDelete('cascade');
+
+            $table->foreign('category_id')
+                ->references('id')->on('phrasebook_categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('phrasebooks');
+    }
+}
