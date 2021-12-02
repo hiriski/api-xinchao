c307589ff59c0dac8c82e9d3ccc60859143268c0
hiriski <hiriski@outlook.com>
2021-12-02 16:37:08 +0700

    Update SocialAccountController.php

--- app/Http/Controllers/Auth/SocialAccountController.php
+++ app/Http/Controllers/Auth/SocialAccountController.php
@@ -29,7 +29,8 @@ class SocialAccountController extends Controller
         ]);
 
         $user = User::where('email', $requestSocialAccount['social_email'])->first();
-        if($user === null) {
+        if ($user === null) {
+            /** create new social account */
             $socialAccount = new SocialAccount;
             $socialAccount->social_id           = $requestSocialAccount['social_id'];
             $socialAccount->social_name         = $requestSocialAccount['social_name'];
@@ -39,20 +40,36 @@ class SocialAccountController extends Controller
             $user = User::create([
                 'name'      => $requestSocialAccount['social_name'],
                 'email'     => $requestSocialAccount['social_email'],
-                'username'  => $requestSocialAccount['social_name'] . Str::random(5)
+                'username'  => $requestSocialAccount['social_id'], /* $requestSocialAccount['social_name'] . Str::random(5) */
             ]);
             $socialAccount->fill(['user_id' => $user->id])->save();
         }
+
+        // update social photo url 
+        if (Str::contains($user->socialAccount->social_photo_url, ["https://lh3.googleusercontent.com/"]) || !$user->socialAccount->social_photo_url) {
+            $newPhotoUrl = null;
+            if (Str::contains($requestSocialAccount['social_photo_url'], '=s96-c')) {
+                $newPhotoUrl = Str::replaceLast('=s96-c', '', $requestSocialAccount['social_photo_url']);
+            } else {
+                $newPhotoUrl = $requestSocialAccount['social_photo_url'];
+            }
+            $user->socialAccount()->update([
+                "social_photo_url" => $newPhotoUrl
+            ]);
+        }
+
         $token = $user->createToken($user->email)->plainTextToken;
-        return $this->responseWithToken($token, $user);
+        return $this->responseWithToken($token, $user, $requestSocialAccount['social_provider']);
     }
 
-    protected function responseWithToken($token, $user) {
+    protected function responseWithToken($token, $user, $provider)
+    {
         return response()->json([
             'success'     => true,
             'token'       => $token,
             'token_type'  => 'bearer',
-            'user'        => new UserResource($user)
+            'user'        => new UserResource($user),
+            'provider'    => $provider,
         ], JsonResponse::HTTP_OK);
     }
 }
